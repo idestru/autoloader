d2bd22ba6f13b793f0ce8f0e9141a139d9ea4b84
Idestru <idestru@yandex.ru>
2015-07-14 09:47:23 +0300

    Added directory separator

--- bootstrap.php
+++ bootstrap.php
@@ -5,5 +5,8 @@ require_once   __DIR__.'/vendor/symfony/class-loader/Psr4ClassLoader.php';
 use Symfony\Component\ClassLoader\Psr4ClassLoader;
 
 $loader = new Psr4ClassLoader();
-$loader->addPrefix('IdestDev', __DIR__.'/src/IdestDev');
+$loader->addPrefix(
+        'IdestDev',
+        __DIR__.DIRECTORY_SEPARATOR.'src'.DIRECTORY_SEPARATOR.'IdestDev'
+);
 $loader->register();
