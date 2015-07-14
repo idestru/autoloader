710b015027c57b9d9800417d5d9d6c64eacbbabd
Idestru <idestru@yandex.ru>
2015-07-14 09:41:52 +0300

    Removed psr0

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,9 @@
 <?php
 
-require_once   __DIR__.'/loaders/psr4symfonyloader.php'; 
-//require_once   __DIR__.'/loaders/psr0symfonyloader.php'; 
+require_once   __DIR__.'/vendor/symfony/class-loader/Psr4ClassLoader.php';
+
+use Symfony\Component\ClassLoader\Psr4ClassLoader;
+
+$loader = new Psr4ClassLoader();
+$loader->addPrefix('IdestDev', __DIR__.'/src/IdestDev');
+$loader->register();
